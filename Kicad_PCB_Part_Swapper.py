0e89b89255cb75ef935b205fde17eaf021bcfeba
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-06-17 23:03:31 +0200

    Added board loading and requested parts finding.

--- Kicad_PCB_Part_Swapper.py
+++ Kicad_PCB_Part_Swapper.py
@@ -10,3 +10,35 @@ if len(sys.argv) != 4:
 firstPartReference = sys.argv[1]
 secondPartReference = sys.argv[2]
 pcbFileName = sys.argv[3]
+
+# Try to open PCB file
+try:
+	pcbBoard = pcbnew.LoadBoard(pcbFileName)
+except:
+	print(f"Error : failed to load PCB file '{pcbFileName}', make sure the file exists.")
+
+# Try to find parts from given references
+firstPartModule = None
+secondPartModule = None
+for module in pcbBoard.GetModules():
+	moduleReference = module.GetReference()
+	
+	# Is this the first module ?
+	if firstPartReference == moduleReference:
+		firstPartModule = module
+		print(f"Found part 1 '{firstPartReference}'.")
+		continue
+	
+	# Is this the second module ?
+	if secondPartReference == moduleReference:
+		secondPartModule = module
+		print(f"Found part 2 '{secondPartReference}'.")
+		continue
+
+# Were both parts found ?
+if firstPartModule is None:
+	print(f"Error : could not find part 1 '{firstPartReference}'.")
+	sys.exit(-1)
+if secondPartModule is None:
+	print(f"Error : could not find part 2 '{secondPartReference}'.")
+	sys.exit(-1)
